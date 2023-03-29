587ac7f1509673733949821b2a915d7173014566
cbysal <1187588638@qq.com>
2023-03-29 16:31:02 +0800

    fix bug on tx hash

--- src/core/types/transaction.cpp
+++ src/core/types/transaction.cpp
@@ -2,4 +2,4 @@
 
 Tx newTx(Id from, Id to, uint16_t nonce) { return (uint64_t(to) << 32) | (uint64_t(from) << 16) | nonce; }
 
-Hash hashTx(Tx tx) { return tx & 0xffff; }
+Hash hashTx(Tx tx) { return tx & 0xffffffff; }
